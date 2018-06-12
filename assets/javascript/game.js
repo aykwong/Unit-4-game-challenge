5f49c3850537a1d16d8f09e72ad295c13f661471
Alpha <aykwong93@gmail.com>
2018-06-11 21:17:18 -0400

    Initial files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,24 @@
 var characters = [
   {
-    name: "obi-wan kenobi",
+    name: "Obi-wan kenobi",
     hp: "120",
     attack: "8",
     cap: "8"
   },
   {
-    name: "luke skywalker",
+    name: "Luke skywalker",
     hp: "100",
     attack: "5",
     cap: "5"
   },
   {
-    name: "yoda",
+    name: "Yoda",
     hp: "150",
     attack: "20",
     cap: "20"
   },
   {
-    name: "darth sidious",
+    name: "Darth sidious",
     hp: "180",
     attack: "25",
     cap: "25"
@@ -27,28 +27,28 @@ var characters = [
 
 var mainCharacter = "";
 var enemyCharacter = "";
-var newSrc = "";
 
 $(".characters").on("click", ".hero", function() {
   console.log($(this).attr("id"));
   for (var i = 0; i < characters.length; i++) {
     if ($(this).attr("id") === characters[i].name) {
       mainCharacter = characters[i];
-      newSrc = $(this).attr("src");
+      let newSrc = $(this).attr("src");
       $("#mainCharacter").css("visibility", "visible");
       $("#mainCharacter").attr("src", newSrc);
 
-      var chosenCharacter = $("<div>");
-      chosenCharacter.addClass("name");
-      chosenCharacter.text(mainCharacter[0]);
-      console.log(chosenCharacter);
-      $(".characters #mainCharacter").append(mainCharacter[0]);
+      let chosenCharacterName = $("<div>");
+      chosenCharacterName.addClass("name");
+      chosenCharacterName.text(mainCharacter.name);
+      console.log(chosenCharacterName);
+      $(".mainzone").append(chosenCharacterName);
 
-      chosenCharacter.removeClass("name");
-      chosenCharacter.addClass("hp");
-      chosenCharacter.text(mainCharacter[1]);
-      console.log(chosenCharacter);
-      $(".characters #mainCharacter .name").append(chosenCharacter);
+      let chosenCharacterHp = $("<div>");
+      chosenCharacterHp.addClass("hp");
+      chosenCharacterHp.text(mainCharacter.hp);
+      $(".mainzone").append(chosenCharacterHp);
+
+      $(".hero").css("display", "none");
     }
   }
 });
